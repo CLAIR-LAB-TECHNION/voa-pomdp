443d05d7315e2c6666cbbb88c30771cbbe4121ff
yuvalgos <yuvalgos@gmail.com>
2024-06-16 14:24:38 +0300

    finaly gripper works. before wrapping code

--- rtde_try.py
+++ rtde_try.py
@@ -3,6 +3,7 @@ from rtde_receive import RTDEReceiveInterface as rtdercv
 from rtde_io import RTDEIOInterface as rtdeio
 from numpy import pi
 import time
+from twofg7_gripper import TwoFG7
 import urx
 
 
@@ -10,6 +11,7 @@ freq = 125
 rtde_c = rtdectrl("192.168.0.10", freq,)
 rtde_r = rtdercv("192.168.0.10", freq)
 rtde_io = rtdeio("192.168.0.10", freq)
+gripper = TwoFG7("192.168.0.10", 0)
 time.sleep(0.5)
 
 
@@ -17,6 +19,7 @@ time.sleep(0.5)
 
 home = [0, -pi/2, 0, -pi/2, 0, 0]
 rtde_c.moveJ(q=home, speed=0.5, acceleration=0.5)
+gripper.twofg_grip_external(35.0, 40, 25)
 
 goal = home.copy()
 goal[0] = pi/5
@@ -29,6 +32,7 @@ rtde_c.moveL(target_pose, speed=0.1, acceleration=0.1)
 
 target_pose[1] -= 0.1
 rtde_c.moveJ_IK(target_pose, speed=0.5, acceleration=0.5)
+gripper.twofg_grip_external(0.0, 40, 25)
 
 rtde_c.moveJ(q=home, speed=0.5, acceleration=0.5)
 
