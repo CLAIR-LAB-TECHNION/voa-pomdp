fbb119045b63f9f7d1ea5cb87700c5be5fa0ff18
yuvalgos <yuvalgos@gmail.com>
2024-06-25 17:17:59 +0300

    robots alingment

--- scripts/align_two_robots.py
+++ scripts/align_two_robots.py
@@ -9,7 +9,7 @@ from robot_inteface.robots_metadata import ur5e_1, ur5e_2
 app = typer.Typer()
 
 mid_point = [-0.4, -0.6]
-offset = 0.15
+offset = 0.08
 
 
 @app.command(
@@ -19,10 +19,10 @@ def main(axis="x",):
     motion_planner = MotionPlanner()
     gt = GeometryAndTransforms.from_motion_planner(motion_planner)
 
-    # r1_controller = ManipulationController(ur5e_1["ip"], ur5e_1["name"], motion_planner, gt)
+    r1_controller = ManipulationController(ur5e_1["ip"], ur5e_1["name"], motion_planner, gt)
     r2_controller = ManipulationController(ur5e_2["ip"], ur5e_2["name"], motion_planner, gt)
-    # r1_controller.speed = 0.3
-    # r1_controller.acceleration = 0.3
+    r1_controller.speed = 0.3
+    r1_controller.acceleration = 0.3
     r2_controller.speed = 0.3
     r2_controller.acceleration = 0.3
 
@@ -36,8 +36,8 @@ def main(axis="x",):
         r1_point[1] += offset
         r2_point[1] -= offset
 
-    # r1_controller.plan_and_move_to_xyzrz(r1_point[0], r1_point[1], 0.2, 0)
-    r2_controller.plan_and_move_to_xyzrz(r2_point[0], r2_point[1], 0.2, 0.8)
+    r1_controller.plan_and_move_to_xyzrz(r1_point[0], r1_point[1], 0.2, 0)
+    r2_controller.plan_and_move_to_xyzrz(r2_point[0], r2_point[1], 0.2, 0)
 
 
 if __name__ == "__main__":
