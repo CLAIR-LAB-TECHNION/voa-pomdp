16badcfc5f67bbf2f6a8cb760c5cce39dc3f9fca
yuvalgos <yuvalgos@gmail.com>
2024-07-08 17:38:24 +0300

    added coarse error computation

--- scripts/object_detection_cropped_images.py
+++ scripts/object_detection_cropped_images.py
@@ -29,6 +29,32 @@ def load_data():
     return robot_configs, images, depth_images, actual_block_positions
 
 
+def compute_prediction_error(pred, actual):
+    """
+    Compute the prediction error between the predicted and the actual block positions,
+    each prediction is assosiated with nearest actual block position
+    :param pred: predicted block positions nx3 array
+    :param actual: actual block positions nx3 array
+    :return: the mean error
+    """
+    pred = np.array(pred)
+    actual = np.array(actual)
+
+    # there might be less pred than actual. In that case, we will just ignore the extra actuals.
+    # compute the error between every pred and actual
+    errors = []
+    for pred_pos in pred:
+        # find the nearest actual block position
+        distances = np.linalg.norm(actual[:, :2] - pred_pos[:2], axis=1)
+        nearest_idx = np.argmin(distances)
+        nearest_actual = actual[nearest_idx]
+
+        error = np.linalg.norm(nearest_actual[:2] - pred_pos[:2])
+        errors.append(error)
+
+    return errors
+
+
 if __name__ == "__main__":
     workspace_limits_x = [-0.9, -0.54]
     workspace_limits_y = [-1.0, -0.55]
@@ -44,8 +70,10 @@ if __name__ == "__main__":
 
     actual_block_positions = np.array(actual_block_positions)
 
-
     for pred, annotations in zip(block_positions, annotations):
+        error = compute_prediction_error(pred, actual_block_positions)
+        print(f"error {error}, mean error {np.mean(error)}")
+
         # create plot with four subplots, 3 for the given annotations and one for the predicted block positions as below
         fig, axs = plt.subplots(2, 2)
         axs[0, 0].imshow(annotations[0])
@@ -61,5 +89,4 @@ if __name__ == "__main__":
         axs[1, 1].set_ylim(workspace_limits_y)
 
         fig.tight_layout()
-
         plt.show()
