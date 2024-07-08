b2474df818660e4aa9f5c05c13f1b96052d3e043
yuvalgos <yuvalgos@gmail.com>
2024-07-08 21:40:50 +0300

    moved pyrender example to script

--- vision/state_renderer.py
+++ vision/state_renderer.py
@@ -1,4 +1,3 @@
-import json
 import numpy as np
 import pyrender
 import trimesh
@@ -7,29 +6,9 @@ from motion_planning.geometry_and_transforms import GeometryAndTransforms
 from camera.configurations_and_params import color_fx, color_fy, color_ppx, color_ppy
 
 
-def load_data():
-    metadata = json.load(open("images_data_merged_hires/merged_metadata.json", "r"))
-    robot_configs = []
-    images = []
-    depth_images = []
-    actual_block_positions = []
-
-    for im_metadata in metadata:
-        robot_configs.append(im_metadata["ur5e_1_config"])
-
-        image_path = f"images_data_merged_hires/images/{im_metadata['image_rgb']}"
-        image = np.load(image_path)
-        images.append(image)
-
-        depth_images.append(np.load(f"images_data_merged_hires/depth/{im_metadata['image_depth']}"))
-
-    # actual block positions are the same for all images, so we can just take the first one
-    actual_block_positions = metadata[0]["block_positions"]
-
-    return robot_configs, images, depth_images, actual_block_positions
-
-
 class PyrenderRenderer:
+    box_size = [0.04, 0.04, 0.04]
+
     def __init__(self, gt: GeometryAndTransforms = None):
         self.gt = gt if gt is not None else GeometryAndTransforms.build()
         self.scene = pyrender.Scene(bg_color=[0, 0, 0, 0], ambient_light=[1.0, 1.0, 1.0, 1.0])
@@ -55,7 +34,7 @@ class PyrenderRenderer:
             self.scene.remove_node(node)
 
         for coord in positions:
-            box_mesh = trimesh.creation.box(extents=box_size)
+            box_mesh = trimesh.creation.box(extents=self.box_size)
             box_mesh.visual.face_colors = [0, 255, 0, 255]
             box_mesh.apply_translation(coord)
             material = pyrender.MetallicRoughnessMaterial(
@@ -83,29 +62,3 @@ class PyrenderRenderer:
                                             [0, 0, 0, 1]])
         return camera_pose @ opengl_camera_transform
 
-
-box_size = [0.04, 0.04, 0.04]
-plane_size = [0.84, 1.85, 0.01]
-plane_position = [-0.805, -0.615, 0]
-
-if __name__ == "__main__":
-    robot_configs, images, depth_images, actual_block_positions = load_data()
-
-    box_3d_positons = [(p[0], p[1], 0.01) for p in actual_block_positions]
-
-    r = PyrenderRenderer()
-    r.set_boxes_positions(box_3d_positons)
-
-    for robot_config, image in zip(robot_configs, images):
-        rendered_color, depth = r.render_from_robot_config("ur5e_1", robot_config)
-
-        # Extract the green boxes and black edges
-        mask = rendered_color[:, :, 3] > 0  # Mask where alpha > 0
-        overlay = np.zeros_like(rendered_color)
-        overlay[mask] = rendered_color[mask]
-
-        # plot rendered_color on top of image, treat transparent background of rendered as transparent
-        plt.imshow(image)
-        plt.imshow(overlay, alpha=0.5)
-        plt.axis('off')
-        plt.show()
