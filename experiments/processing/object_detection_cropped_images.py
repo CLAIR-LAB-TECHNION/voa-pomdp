3893193a896ca28bce7c0731177db5e2bf91dda4
yuvalgos <yuvalgos@gmail.com>
2024-07-09 10:58:26 +0300

    reorder repository

--- experiments/processing/object_detection_cropped_images.py
+++ experiments/processing/object_detection_cropped_images.py
@@ -8,7 +8,7 @@ from vision.image_block_position_estimator import ImageBlockPositionEstimator
 
 
 def load_data():
-    metadata = json.load(open("../vision/images_data_merged_hires/merged_metadata.json", "r"))
+    metadata = json.load(open("../../vision/images_data_merged_hires/merged_metadata.json", "r"))
     robot_configs = []
     images = []
     depth_images = []
@@ -17,11 +17,11 @@ def load_data():
     for im_metadata in metadata:
         robot_configs.append(im_metadata["ur5e_1_config"])
 
-        image_path = f"../vision/images_data_merged_hires/images/{im_metadata['image_rgb']}"
+        image_path = f"../../vision/images_data_merged_hires/images/{im_metadata['image_rgb']}"
         image = np.load(image_path)
         images.append(image)
 
-        depth_images.append(np.load(f"../vision/images_data_merged_hires/depth/{im_metadata['image_depth']}"))
+        depth_images.append(np.load(f"../../vision/images_data_merged_hires/depth/{im_metadata['image_depth']}"))
 
     # actual block positions are the same for all images, so we can just take the first one
     actual_block_positions = metadata[0]["block_positions"]
