d3cc4566a6fcefc9eed5558e1b4957495ca798df
yuvalgos <yuvalgos@gmail.com>
2024-07-24 11:27:44 +0300

    commit before decision to probably solve as BMDP

--- modeling/pomdp/pomdp_py_domain.py
+++ modeling/pomdp/pomdp_py_domain.py
@@ -3,19 +3,25 @@ import numpy as np
 
 
 class State(pomdp_py.State):
-    def __init__(self, block_positions, steps_left):
+    def __init__(self, block_positions, steps_left, last_stack_attempt_succeded=None):
         self.block_positions = np.asarray(block_positions)
         self.steps_left = steps_left
+
+        # this is meant to hold the information about success after stack attempt.
+        # we need it because the observation depends only on next state on pomdp_py
+        # and we need to know previous state. this is a small hack to overcome this.
+        self.last_stack_attempt_succeded = None
+
     def __eq__(self, other):
         return self.block_positions == other.block_positions and self.steps_left == other.steps_left
 
     def __hash__(self):
-        return hash((self.block_positions, self.steps_left))
+        return hash((self.block_positions, self.steps_left, self.last_stack_attempt_succeded))
 
 
 class ObservationBase(pomdp_py.Observation):
     def __init__(self, steps_left):
-        self.steps_left = steps_left
+        self.steps_left = steps_left  # this is always fully observable
 
     def __eq__(self, other):
         raise NotImplementedError
@@ -37,6 +43,7 @@ class ObservationSenseResult(ObservationBase):
 
     def __hash__(self):
         return hash((self.x, self.y, self.is_occupied, self.steps_left))
+
 
 class ObservationStackAttemptResult(ObservationBase):
     def __init__(self, is_object_picked, steps_left):
@@ -62,12 +69,12 @@ class ActionBase(pomdp_py.Action):
         return hash((self.x, self.y))
 
 
-class ActionPickUp(ActionBase):
+class ActionAttemptStack(ActionBase):
     def __init__(self, x, y):
         super(ActionBase).__init__(x, y)
 
     def __eq__(self, other):
-        return isinstance(other, ActionPickUp) and self.x == other.x and self.y == other.y
+        return isinstance(other, ActionAttemptStack) and self.x == other.x and self.y == other.y
 
 
 class ActionSense(ActionBase):
