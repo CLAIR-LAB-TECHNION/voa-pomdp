started to setup camera rendering and found out that robots rotation is wrong in simulator.

experiments_sim/block_stacking_simulator.py
@@ -122,10 +122,14 @@ class BlockStackingSimulator:
         return observation, reward
 
 
-    def sense_camera(self, camera_position, camera_orientation):
-        # success, _, _ = self.motion_executor.move_to_pose(agent, camera_position, camera_orientation)
-        # if success:
-        return self.mujoco_env.render_image_from_pose(np.array(camera_position), np.array(camera_orientation))
+    def sense_camera_r1(self, robot_config):
+        self.motion_executor.plan_and_moveJ("ur5e_1", robot_config)
+
+        actual_config = self.mujoco_env.robots_joint_pos["ur5e_1"]
+        camera_pose = self.motion_executor.motion_planner.get_forward_kinematics("ur5e_1", actual_config)
+        position = np.array(camera_pose[1])
+        orientation = np.array(camera_pose[0]).reshape(3, 3)
+        return self.mujoco_env.render_image_from_pose(position, orientation)
 
     def move_r2_above_tower(self):
         self.motion_executor.plan_and_move_to_xyz_facing_down("ur5e_2",

multi_mujoco/motion_planning/motion_executor.py
@@ -55,17 +55,18 @@ class MotionExecutor:
 
         state = self.env.get_state()
 
-        # set current configuration
-        for robot, pose in state['robots_joint_pos'].items():
-            self.motion_planner.update_robot_config(robot, pose)
-        # for block_name, pos in blocks_positions_dict.items():
-        #     self.motion_planner.add_block(name=block_name, position=pos)
+        self.update_all_robots_configs_in_mp()
 
         self.time_step = self.env._mj_model.opt.timestep * self.env.frame_skip
 
         # self.pid_controllers = {}
         # for robot in self.env.robots_joint_pos.keys():
         #     self.pid_controllers[robot] = [PIDController(kp=1.0, ki=0.1, kd=0.05) for _ in range(6)]
+
+    def update_all_robots_configs_in_mp(self):
+        state = self.env.get_state()
+        for robot, pose in state['robots_joint_pos'].items():
+            self.motion_planner.update_robot_config(robot, pose)
 
     def moveJ(self, robot_name, target_joints, speed=1.0, acceleration=1.0, tolerance=0.003):
         self.zero_all_robots_vels_except(robot_name)
@@ -119,11 +120,10 @@ class MotionExecutor:
             if np.allclose(current_joints, target_joints, atol=tolerance):
                 break
 
-        # TODO: Handle cases where target joints are unreachable
+        self.update_all_robots_configs_in_mp()
 
         if step == max_steps - 1:
-            # TODO: Log that the movement timed out
-            pass
+            logging.warning(f"Movement for {robot_name} timed out before reaching the target joints.")
 
     def generate_smooth_trajectory(self, start, end, num_steps):
         t = np.linspace(0, 1, num_steps)
@@ -173,6 +173,8 @@ class MotionExecutor:
 
         # Execute the full trajectory
         self.execute_trajectory(robot_name, full_trajectory, tolerance)
+
+        self.update_all_robots_configs_in_mp()
 
     def generate_trajectory(self, start_config, end_config, speed, acceleration, blend_radius, blend_start=True,
                             blend_end=True):

multi_mujoco/scratch.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from matplotlib import pyplot as plt
 from experiments_sim.block_stacking_simulator import BlockStackingSimulator
 from modeling.pomdp_problem.domain.action import ActionSense, ActionAttemptStack
 
@@ -11,12 +11,18 @@ blocks_pos = [[-0.8, -0.8],
 
 simulator = BlockStackingSimulator(visualize_mp=False, max_steps=7, render_sleep_to_maintain_fps=False)
 
+help_configs = np.load("../experiments_lab/configurations/help_configs.npy")
+
 for i in range(10):
     blocks_pos[0][0] += 0.01
     print("resetting with blocks_pos", blocks_pos)
     simulator.reset(block_positions=blocks_pos)
-    simulator.motion_executor.plan_and_moveJ("ur5e_1", [+1.0776e+00, -1.57137098e+00, 1.06499581e-04, -1.57132691e+00,
-                                                        -2.73702160e-06, 4.52766e-07])
+
+
+    im = simulator.sense_camera_r1([-0.5567808869537451, -1.215127556940363, -1.8444974285294637, 1.258726315197987, -0.6591467161558847, -0.20351355710407049])
+    plt.imshow(im)
+    plt.show()
+
     obs = None
 
     while True: