123fd0950827b107007c9e7924458cd706cd87c1
yuvalgos <yuvalgos@gmail.com>
2024-09-17 16:35:21 +0300

    toward block position estimation in simulation with the lab position estimator. Need to debug

--- multi_mujoco/scratch.py
+++ multi_mujoco/scratch.py
@@ -1,16 +1,32 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from experiments_sim.block_stacking_simulator import BlockStackingSimulator
+from lab_ur_stack.motion_planning.geometry_and_transforms import GeometryAndTransforms
+from lab_ur_stack.vision.utils import detections_plots_no_depth_as_image
 from modeling.pomdp_problem.domain.action import ActionSense, ActionAttemptStack
+
+
+from lab_ur_stack.utils.workspace_utils import workspace_x_lims_default, workspace_y_lims_default
+from lab_ur_stack.vision.image_block_position_estimator import ImageBlockPositionEstimator
+
 
 blocks_pos = [[-0.8, -0.8],
               [-0.8, -0.7],
               [-0.65, -0.72],
               [-0.6, -0.6],]
 
+# blocks in 4 corners of the workspace:
+blocks_pos = [[workspace_x_lims_default[0], workspace_y_lims_default[0]],
+                [workspace_x_lims_default[0], workspace_y_lims_default[1]],
+                [workspace_x_lims_default[1], workspace_y_lims_default[0]],
+                [workspace_x_lims_default[1], workspace_y_lims_default[1]]]
 
-simulator = BlockStackingSimulator(visualize_mp=False, max_steps=7, render_sleep_to_maintain_fps=True,
-                                   render_mode="human")
+simulator = BlockStackingSimulator(visualize_mp=False, max_steps=7, render_sleep_to_maintain_fps=False,
+                                   render_mode='human')
+
+gt = GeometryAndTransforms(simulator.motion_executor.motion_planner, camera_in_ee=np.array(simulator.helper_camera_translation_from_ee))
+position_estimator = ImageBlockPositionEstimator(workspace_x_lims_default, workspace_y_lims_default, gt,"ur5e_1",
+                                                 intrinsic_camera_matrix=simulator.mujoco_env.get_robot_cam_intrinsic_matrix())
 
 help_configs = np.load("../experiments_lab/configurations/help_configs.npy")
 
@@ -19,9 +35,17 @@ for i in range(10):
     print("resetting with blocks_pos", blocks_pos)
     simulator.reset(block_positions=blocks_pos)
 
+    im, actual_config_for_im = simulator.sense_camera_r1([-0.5567808869537451, -1.215127556940363, -1.8444974285294637, 1.258726315197987, -0.6591467161558847, -0.20351355710407049])
+    pred_positions, annotations = position_estimator.get_block_position_plane_projection(im, actual_config_for_im,
+                                                                                    plane_z=0.025)
+    detections_plot = detections_plots_no_depth_as_image(annotations[0], annotations[1], pred_positions,
+                                                         workspace_x_lims_default, workspace_y_lims_default,
+                                                         actual_positions=blocks_pos)
 
-    im = simulator.sense_camera_r1([-0.5567808869537451, -1.215127556940363, -1.8444974285294637, 1.258726315197987, -0.6591467161558847, -0.20351355710407049])
-    plt.imshow(im)
+    plt.figure(dpi=512, tight_layout=True, figsize=(5,10))
+
+    plt.imshow(detections_plot)
+    plt.axis('off')
     plt.show()
 
     obs = None
