611b18f6db684044f2f06c427c2b2c50ec6cf639
yuvalgos <yuvalgos@gmail.com>
2024-11-29 15:24:38 +0200

    started implementing VOA prediction with function left to fill

--- modeling/voa_predictions/observation_prediction_functions.py
+++ modeling/voa_predictions/observation_prediction_functions.py
@@ -6,10 +6,25 @@ from modeling.sensor_distribution import detections_to_distributions, compute_po
 
 def sample_observation_fov_based(block_pos_state,
                                  help_config,
-                                 gt,
+                                 gt: GeometryAndTransforms,
                                  cam_intrinsic_matrix,
                                  detection_probability=0.9,
                                  margin_in_pixels=30,):
+    """
+    sample observation given the block positions and the helper configuration.
+    assuming blocks that are within fov of the camera are detected with a probability of detection_probability,
+    ignoring occulusions.
+    the detected mus for the positins have added noise base on the same model as in detections_to_distributions
+
+    @param block_pos_state: state (list of block positions)
+    @param help_config: config of robot with the camera
+    @param gt: GeometryAndTransforms object for that scene
+    @param cam_intrinsic_matrix: camera intrinsic matrix
+    @param detection_probability: probability of detecting blocks that are in fov
+    @param margin_in_pixels: blocks that are in fov but close to the edge by this margin in one of the axes,
+        are considered out of fov
+    @return:
+    """
 
     block_pos_3d = np.concatenate([block_pos_state, 0.025 * np.ones((len(block_pos_state), 1))], axis=1)
     blocks_in_fov = in_r1_fov(block_pos_3d, help_config, gt, cam_intrinsic_matrix, margin_in_pixels=margin_in_pixels)
